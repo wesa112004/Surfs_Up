a949e0401ef02e3994b2c1a57cfa93593fdf0f7e
Wes Adams <wesadams@Wess-MacBook-Pro.local>
2022-04-24 14:04:35 -0700

    Surfs Up

--- app.py
+++ app.py
@@ -1,5 +1,42 @@
-from flask import Flask 
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
+
+
+from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+ 
+ # Precipitation Route
+ 
+@app.route('/api/v1.0/precipitation')   
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
+    
